1f37faba0c54b2d055d21aba6dc8de370065632d
mahdizarei <mahdizarei0614@gmail.com>
2024-06-06 17:13:34 +0330

    configured GitHub actions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./components/main-container/main-container.component').then((c) => c.MainContainerComponent)
+  }
+];
